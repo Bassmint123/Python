f475673646e3fb77191448b41342864c6f3ad98a
Brad Beiermann <bradb@cimstrat.com>
2016-07-03 10:46:39 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,10 @@
+# Testing assigned variables.
+x = 5
+y = 7
+z = x + y
+print ("z is equal to", z, "that's all. \nThanks!")
+
+# Testing conditional flow control in Python
+max = x if(x > y) else y
+print( '\nGreater value is', max)
+
