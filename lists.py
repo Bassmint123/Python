e7050cf690da1bdfe8c7a2aa8a4965c5cd03d243
Brad Beiermann <bradb@cimstrat.com>
2016-07-29 18:39:53 -0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Initialize a list of three elements
+quarter = ['January', 'February', 'Marching']
+
+print('First Month:', quarter[0])
+print('Second Month:', quarter[1])
+print('Third Month:', quarter[2])
+
+day = [' Monday',' Happy Day',' Great Day']
+
+print('Month and Day is:', quarter[2], day[1])
+
