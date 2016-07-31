3ce117183142b2db92b872717c533f415f8afde1
Brad Beiermann <bradb@cimstrat.com>
2016-07-30 21:05:37 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -5,7 +5,7 @@ print('First Month:', quarter[0])
 print('Second Month:', quarter[1])
 print('Third Month:', quarter[2])
 
-day = [' Monday',' Happy Day',' Great Day']
+day = [' Monday',' Happy Day',' A Great Day']
 
 print('Month and Day is:', quarter[2], day[1])
 
