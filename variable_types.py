228a9bb317c6ddc36c28b48fd07f8d740c6786f7
Brad Beiermann <bradb@cimstrat.com>
2019-06-04 21:14:17 -0500

    Add files via upload

--- variable_types.py
+++ variable_types.py
@@ -7,3 +7,21 @@ z = 5j
 print(type(x))
 print(type(y))
 print(type(z))
+
+# Conversions
+age = 13
+print "I am " + str(age) + " years old!"
+
+# Convert to an integer
+number1 = "100"
+number2 = "10"
+
+string_addition = number1 + number2 
+# string_addition now has a value of "10010"
+
+int_addition = int(number1) + int(number2)
+# int_addition has a value of 110
+
+string_num = "7.5"
+print int(string_num)
+print float(string_num)
