ef8c9559490c848aa37942c2b2acde1a33ac889a
Brad Beiermann <bradb@cimstrat.com>
2019-06-04 20:56:32 -0500

    Add files via upload

---
+++ string_snips.py
@@ -0,0 +1,15 @@
+# Appending a string
+print("This is a string " + "that has been appended")
+
+# Using triple quotes for a multi-line string
+haiku = """The old pond,
+A frog jumps in: 
+Plop!"""
+
+print haiku
+
+"""
+This is an inline
+comment that can be used
+anywhere in the code.
+"""
