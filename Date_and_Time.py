33aee89e9e014b8fde9aaf350cb02dcee9111f6f
Brad Beiermann <bradb@cimstrat.com>
2019-06-09 22:39:25 -0500

    Add files via upload

---
+++ Date_and_Time.py
@@ -0,0 +1,12 @@
+# Getting the Current Date and Time
+from datetime import datetime
+now = datetime.now()
+
+# Get current time, day, year
+print now
+# Get current year
+print now.year
+# Get current numerical month
+print now.month
+# Get current day of the month
+print now.day
