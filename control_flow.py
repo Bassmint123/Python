13020bc49771afd1cbb3b53c4841cea70d0278b0
Brad Beiermann <bradb@cimstrat.com>
2019-06-11 17:05:32 -0500

    Add files via upload

---
+++ control_flow.py
@@ -0,0 +1,41 @@
+
+# Gonna be using <, !=, >, <=, ==, >= in the next few lines
+
+bool_one = 3 < 5  # Evaluates to True  
+
+bool_two = 100 != 100 # Evaluates to False
+
+bool_three = 15 > 10 # Evaluates to True
+
+bool_four = 34 < 5 # Evaluates to False
+
+bool_five = 14 <= 15 # Evaluates to True
+
+bool_six = 5 == 5 # Evaluates to True
+
+bool_seven = 6 >= 5 # Evaluates to True
+
+# Gonna do some booleans here using AND
+
+bool_one = False and False 
+print (bool_one) # Evaluates to False
+
+bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
+
+bool_three = 19 % 4 != 300 / 10 / 10 and False
+
+bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
+
+bool_five = True and True 
+print (bool_five) # Evaluates to True
+
+# Gonna do some booleans here using OR
+
+bool_one = 100 ** 0.5 >= 50 or False
+print (bool_one)
+
+bool_two = False or False
+print (bool_two)
+
+
+
