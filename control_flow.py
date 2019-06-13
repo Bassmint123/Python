7cd995e87caca181dcae7d27a8b324c1dbb52478
Brad Beiermann <bradb@cimstrat.com>
2019-06-13 14:23:28 -0500

    Add files via upload

--- control_flow.py
+++ control_flow.py
@@ -37,5 +37,17 @@ print (bool_one)
 bool_two = False or False
 print (bool_two)
 
+# The order of boolean evaluation is: not, and, or
+bool_one = False or not True and True
+print(bool_one)
+bool_two = False and not True or True
+print(bool_two)
+bool_three = True and not (False or False)
+print(bool_three)
+bool_four = not not True or False and not True
+print(bool_four)
+bool_five = False or not (True and True)
+print(bool_five)
+
 
 
